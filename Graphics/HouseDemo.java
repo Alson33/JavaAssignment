fea857852c8c1be7f853a725dbb1c9956a145b8a
Salipa-Gurung <salipagurung@gmail.com>
2020-03-15 17:32:06 +0545

    used polygon for shorter and easier code

--- Graphics/HouseDemo.java
+++ Graphics/HouseDemo.java
@@ -5,53 +5,56 @@ class HouseDemo extends Canvas
 {
 	public void paint(Graphics g)
 	{
-		// main bulding rectangle
-		g.drawRect(100, 300, 400, 300);
-		// windows
-		g.drawRect(150, 350, 70, 70);
-		g.drawRect(380, 350, 70, 70);
-		// door
-		g.drawRect(250, 430, 100, 170);
+		// background color for sky
+		g.setColor(Color.BLUE);
+		g.fillRect(0,0, 800, 400);
 
-		// roof
-		g.drawLine(300, 50, 100, 300);
-		g.drawLine(300, 50, 500, 300);
-		
-		// coloring roof
-		// g.setColor(Color.GREEN);
-		// g.fillArc(100, 5, 300, 500, 225, 90);  
+		// background color for land
+		g.setColor(Color.GREEN);
+		g.fillRect(0,400, 800, 400);
 
-		// coloring building
-		g.setColor(Color.RED);
-		g.fillRect(100, 300, 400, 300);
-
-		// coloring the window and door
+		// ovals for cloud
 		g.setColor(Color.WHITE);
-		g.fillRect(150, 350, 70, 70);
-		g.fillRect(380, 350, 70, 70);
+		// g.drawOval(600, 20, 50, 50);
+		g.fillOval(600, 30, 90, 90);
+		g.fillOval(660, 20, 60, 60);
+		g.fillOval(660, 60, 70, 60);
+		g.fillOval(700, 30, 70, 70);
 
-		g.fillRect(250, 430, 100, 170);
 
-		// color for the rims
 		g.setColor(Color.BLACK);
+		// corodinates for the house outer body
+		int x[] = {315, 80, 550, 120, 120, 250, 250, 350, 350, 500, 500, 550, 315};
+		int y[] = {50, 300, 300, 300, 600, 600, 450, 450, 600, 600, 300, 300, 50};
 
-		// window rims
-		g.drawLine(160, 350, 160, 420);
-		g.drawLine(180, 350, 180, 420);
-		g.drawLine(200, 350, 200, 420);
-		g.drawLine(400, 350, 400, 420);
-		g.drawLine(420, 350, 420, 420);
-		g.drawLine(440, 350, 440, 420);
-	}	
+			// outer body of the house
+			// g.drawPolygon(x, y, 13);
+			g.setColor(Color.RED);
+			g.fillPolygon(x, y, 13);
+
+			g.setColor(new Color(155, 105, 35));
+			g.fillPolygon(x, y, 3);
+
+			// windows of the house
+			g.setColor(Color.GRAY);
+			// g.drawRect(150, 350 , 70, 70);
+			// g.drawRect(390, 350, 70, 70);
+			g.fillRect(150, 350 , 70, 70);
+			g.fillRect(390, 350, 70, 70);
+
+			// door of the house
+			g.fillRect(250, 450, 100, 150);
+
+	}
 
 	public static void main(String[] args) 
 	{
-		HouseDemo ob = new HouseDemo();
+		HouseDemo h = new HouseDemo();
+		Frame f = new Frame();
 
-		JFrame f = new JFrame();
-
-			f.add(ob);
 			f.setSize(800, 800);
 			f.setVisible(true);
+			f.add(h);	
+			// f.setBackground(Color.GREEN);
 	}
 }
